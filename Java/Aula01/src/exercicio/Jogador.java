d695c6060def76cfc2e8b6b21d297d86c8708248
Neemias Casonatto Policarpi <69010841+policarpi@users.noreply.github.com>
2021-07-03 12:00:38 -0300

    Execiciuo jogador

---
+++ Java/Aula01/src/exercicio/Jogador.java
@@ -0,0 +1,28 @@
+package exercicio;
+
+import java.util.Scanner;
+
+public class Jogador {
+
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o nome do jogador:  ");
+		String jogador = sc.next();
+		System.out.print("Dite a idade jogador:  ");
+		int idade = sc.nextInt();
+		String mensagemFinal = jogador;
+		
+			if (idade >= 10 & idade <= 12  ) {
+				mensagemFinal += " têm vaga no Infanto-Juvenil";
+				}else if (idade >= 16 & idade <= 18 ) {
+					mensagemFinal += " tem vaga no Aspirante";
+						}else if (idade >= 20 & idade <= 35 ) {
+							mensagemFinal += " tem vaga no Profissional";
+								}else {
+									mensagemFinal += " para sua idade não temos vaga";
+								}	
+				System.out.println(mensagemFinal);
+		sc.close();
+	}
+}
+
