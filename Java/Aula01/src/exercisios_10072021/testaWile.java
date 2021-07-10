e7982de45f95b7cb0a75e5b4d33c7a7f01698988
Neemias Casonatto Policarpi <69010841+policarpi@users.noreply.github.com>
2021-07-10 10:28:13 -0300

    Aula 2

---
+++ Java/Aula01/src/exercisios_10072021/testaWile.java
@@ -0,0 +1,54 @@
+package exercisios_10072021;
+
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class testaWile {
+	
+	public static void main(String[] args) {
+		
+	
+	Scanner sc = new Scanner(System.in);
+	int maiorNumero  = 0, qntImpar =0, qntPar = 0;
+	String numerosInformados = "";
+	double numeroInformado;
+	
+	   JFrame frame = new JFrame("JOptionPane exemplo");
+	   // Cria o JOptionPane por showMessageDialog
+	
+	do {
+		System.out.print("Informe um numero qualquer ");
+		numeroInformado = sc.nextDouble();
+
+			if (numeroInformado % 2 != 0) {
+				qntImpar ++;
+				} else {
+					qntPar ++;
+				}
+					if(numerosInformados.trim() != "") {
+						numerosInformados += ",";
+					}
+						numerosInformados += numeroInformado;
+					
+	} while (numeroInformado != 0);
+	
+//	System.out.println("Numeros informados: " + numerosInformados);
+//	System.out.println("Numeros impar: " + qntImpar);
+//	System.out.println("Numeros Par: " + qntPar);
+		JOptionPane.showMessageDialog(frame,
+	        "\n Numeros par : " + qntPar + 
+	        "\n Numeros impar " + qntImpar +  
+	        "\n Numeros informados: " + numerosInformados + 
+	        "", "Execio testa wile", 
+	        JOptionPane.INFORMATION_MESSAGE);
+	    System.exit(0);
+	
+	
+	
+	sc.close();	
+	}
+
+}
